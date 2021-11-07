5aab047862c1126e7cf50266b50dbcf7ada1b3c7
Crymison <gabrielpaleta@hotmail.com>
2021-11-07 09:14:28 -0300

    Primeiro commit: valor < 2000 fail 2

--- test/MainTest.test.js
+++ test/MainTest.test.js
@@ -8,7 +8,7 @@ describe('Prova', () =>{
         // fatura.valor = 1000;
         // fatura.data =  2; //mes
         
-        const faturas =
+        const fturas =
         [
             {codigo: 54654, valor: 2600, data: 2, id: 1},
             {codigo: 54655, valor: 1600, data: 5, id: 2},
@@ -18,15 +18,15 @@ describe('Prova', () =>{
             {codigo: 54659, valor: 1800, data: 8, id: 6}
         ]
 
-        const clientes = 
-        [
-            {nome: 'Jorel', data: 5, estado: 'Para', fatura: 1},
-            {nome: 'Jose', data: 2, estado: 'Amazonas', fatura: 2},
-            {nome: 'Abigail', data: 6, estado: 'Pernambuco', fatura: 3},
-            {nome: 'Guilherme', data: 11, estado: 'Santa Catarina', fatura: 4},
-            {nome: 'Loriane', data: 1, estado: 'Sao Paulo', fatura: 5},
-            {nome: 'Lucas', data: 4, estado: 'Parana', fatura: 6}
-        ]
+        // const clientes = 
+        // [
+        //     {nome: 'Jorel', data: 5, estado: 'Para', fatura: 1},
+        //     {nome: 'Jose', data: 2, estado: 'Amazonas', fatura: 2},
+        //     {nome: 'Abigail', data: 6, estado: 'Pernambuco', fatura: 3},
+        //     {nome: 'Guilherme', data: 11, estado: 'Santa Catarina', fatura: 4},
+        //     {nome: 'Loriane', data: 1, estado: 'Sao Paulo', fatura: 5},
+        //     {nome: 'Lucas', data: 4, estado: 'Parana', fatura: 6}
+        // ]
 
         const res = Filtrarfraturas(faturas);
         
